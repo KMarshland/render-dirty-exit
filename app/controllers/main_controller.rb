6803905e080d8fd3214dd1f47b12862c1c919f76
Kai Marshland <kaimarshland@gmail.com>
2021-03-02 22:37:40 -0800

    Deploy

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,9 @@
-require_relative '../../lib/rails_crasher/lib/rails_crasher'
-require_relative '../../lib/rails_crasher/ext/rails_crasher/rails_crasher.so'
+if Rails.env.development?
+  require_relative '../../lib/rails_crasher/lib/rails_crasher'
+  require_relative '../../lib/rails_crasher/ext/rails_crasher/rails_crasher.so'
+else
+  require '/opt/render/project/.gems/gems/rails_crasher-0.1.0/lib/rails_crasher'
+end
 
 class MainController < ApplicationController
   def home
